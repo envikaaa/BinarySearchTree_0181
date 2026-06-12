717fb12d510bb510ed2e0f2005a77fef810fc324
envikaaa <envikapriyatna31@gmail.com>
2026-06-12 15:15:04 +0700

    memberi nilai untuk setiap object

--- BinarySearchTree_0181.cpp
+++ BinarySearchTree_0181.cpp
@@ -31,6 +31,8 @@ public:
 
     //membuat prosedur insert
     void insert(string element) {
+        //memberi nilai untuk setiap object yang berada di dalam prosedur insert
+        Node* newNode = new Node(element, nullptr, nullptr);
         
     }
 }
