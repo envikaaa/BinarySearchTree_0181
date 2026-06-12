d0ccd2603a0ac8a1e8d68c52cd2d374c5a34dc66
envikaaa <envikapriyatna31@gmail.com>
2026-06-12 15:19:48 +0700

    membuat conditional statement

--- BinarySearchTree_0181.cpp
+++ BinarySearchTree_0181.cpp
@@ -33,6 +33,14 @@ public:
     void insert(string element) {
         //memberi nilai untuk setiap object yang berada di dalam prosedur insert
         Node* newNode = new Node(element, nullptr, nullptr);
-        
+        newNode->info = element;
+        newNode->leftChild = nullptr;
+        newNode->rightChild = nullptr;
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode);
+
+        //membuat conditional statement
     }
 }
