c63b18de8934f22b5a3a1caf73aabacd7b83dd48
envikaaa <envikapriyatna31@gmail.com>
2026-06-12 15:09:12 +0700

    memberi nilai parameter

--- BinarySearchTree_0181.cpp
+++ BinarySearchTree_0181.cpp
@@ -12,6 +12,9 @@ public:
 
     //membuat constructor untuk class node
     Node(string i, Node* 1, Node* r) {
-        
+        //memberi nilai parameter ke variabel
+        info = i;
+        lefChild = 1;
+        rightChild = r;
     }
 }
